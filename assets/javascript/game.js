6dcffda1d552d6ffe1669d8a3b2dc62141a2a9c2
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-27 17:57:08 -0700

    planned out my pseudocode for this assignment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,8 @@
-console.log("hello")
+//game generates a random "match" number with a value of 19-120
+//game generates 4 crystals with a value of 1-12
+//a new match number is generated when we win or lose
+//when you click a crystal it should add the previous result until it is the same as match
+//if the combined clicks are more than the random result you get 1 loss
+//if the combined clicks are the same as the match number you get 1 win
+//each time you get a win or a loss reset the game for a new match and crystal values
+
