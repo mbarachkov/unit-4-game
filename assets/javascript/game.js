4146f83e1a24710d77e09e453901c0904a2276ef
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-28 18:45:10 -0700

    got a random number being generated and the buttons individually getting random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,36 @@
 //game generates a random "match" number with a value of 19-120
-//game generates 4 crystals with a value of 1-12
-//a new match number is generated when we win or lose
+//game has 4 crystals with a value of 1-12
 //when you click a crystal it should add the previous result until it is the same as match
-//if the combined clicks are more than the random result you get 1 loss
-//if the combined clicks are the same as the match number you get 1 win
+//if the combined crystal clicks are more than the random result you get 1 loss
+//if the combined crystal clicks are the same as the match number you get 1 win
 //each time you get a win or a loss reset the game for a new match and crystal values
+
+var wins = 0;
+var losses = 0;
+
+
+
+var randNumber = Math.floor(Math.random() * 101) + 19;
+$(".guessedNumber").html(randNumber);
+$("#random-target").text(randNumber);
+
+
+var gems1 = Math.floor(Math.random() * 12) + 1;
+var gems2 = Math.floor(Math.random() * 12) + 1;
+var gems3 = Math.floor(Math.random() * 12) + 1;
+var gems4 = Math.floor(Math.random() * 12) + 1;
+
+$(".gems1").on("click", function() {
+    alert(gems1);
+});
+$(".gems2").on("click", function() {
+    alert(gems2);
+});
+$(".gems3").on("click", function() {
+    alert(gems3);
+});
+$(".gems4").on("click", function() {
+    alert(gems4);
+});
+
 
