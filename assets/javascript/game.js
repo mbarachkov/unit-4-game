2ab195c40b383ff7653fb3ef6087a84941105091
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-29 09:44:41 -0700

    finished functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,34 +12,62 @@ var score = 0;
 var randNumber = Math.floor(Math.random() * 101) + 19;
 $(".guessedNumber").html(randNumber);
 $("#random-target").html(randNumber);
+console.log(randNumber);
 
 var gems1 = Math.floor(Math.random() * 12) + 1;
 var gems2 = Math.floor(Math.random() * 12) + 1;
 var gems3 = Math.floor(Math.random() * 12) + 1;
 var gems4 = Math.floor(Math.random() * 12) + 1;
 
+function reset() {
+    score = 0;
+    randNumber = Math.floor(Math.random() * 101) + 19;
+    gems1 = Math.floor(Math.random() * 12) + 1;
+    gems2 = Math.floor(Math.random() * 12) + 1;
+    gems3 = Math.floor(Math.random() * 12) + 1;
+    gems4 = Math.floor(Math.random() * 12) + 1;
+    $("#random-target").html(randNumber);
+    $("#added-score").text(score);
+}
+
 $(".gems1").on("click", function() {
     score += parseInt(gems1);
     $("#added-score").text(score);
     console.log(score);
+    game();
 });
 $(".gems2").on("click", function() {
     score += parseInt(gems1);
     $("#added-score").text(score);
     console.log(score);
+    game();
+
 });
 $(".gems3").on("click", function() {
     score += parseInt(gems3);
     $("#added-score").text(score);
     console.log(score);
+    game();
 });
 $(".gems4").on("click", function() {
     score += parseInt(gems4);
     $("#added-score").text(score);
     console.log(score);
+    game();
 });
 
-// if (score === randNumber) {
-//     wins++;
-//     alert("good job!")
-// }
+function game(){
+    console.log(score)
+    if (score === randNumber) {
+        wins++;
+        $("#win").text(wins);
+        alert("good job!");
+        reset();
+    }
+    if (score > randNumber) {
+        $("#loss").text(losses);
+        losses++;
+        alert("NOPE");
+        reset();
+    }
+}
